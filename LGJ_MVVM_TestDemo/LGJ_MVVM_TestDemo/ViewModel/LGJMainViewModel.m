b8c43e8e25bb380d581bc37afb1d15e48379a2e9
刘光军 <740053245@qq.com>
2017-07-27 23:34:02 +0800

    添加另一种可行的解决问题的方案

--- LGJ_MVVM_TestDemo/LGJ_MVVM_TestDemo/ViewModel/LGJMainViewModel.m
+++ LGJ_MVVM_TestDemo/LGJ_MVVM_TestDemo/ViewModel/LGJMainViewModel.m
@@ -13,6 +13,7 @@
 @interface LGJMainViewModel ()
 
 @property (nonatomic, strong) NSMutableArray *listArr;//盛放所有model的数组
+@property (nonatomic, strong) RowModel *oldRowModel;
 
 @end
 
@@ -29,11 +30,11 @@
     
     self.listArr = [NSMutableArray array];
     
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 5; i++) {
         SectionModel *model = [SectionModel new];
         model.sectionName = [NSString stringWithFormat:@"%d", i];
         NSMutableArray *mutArr = [NSMutableArray array];
-        for (int j = 0; j < 20; j++) {
+        for (int j = 0; j < 3; j++) {
             RowModel *rowModel = [RowModel new];
             rowModel.name = [NSString stringWithFormat:@"第%d行", j];
             rowModel.detail = [NSString stringWithFormat:@"我是第%d行， 多多指教", j];
@@ -46,35 +47,65 @@
 
 #pragma mark - change Cell Model
 - (void)changeCellModelWithIndexPath:(NSIndexPath *)indexPath {
-//    RowModel *rm = [RowModel new];
-//    rm.name = @"新替换的name";
-//    rm.detail = @"新替换的detail";
+    
+    //1、行不通
+//    RowModel *newRowModel = [RowModel new];
+//    newRowModel.name = @"新替换的name";
+//    newRowModel.detail = @"新替换的detail";
 //    
 //    SectionModel *sModel = [SectionModel new];
 //    sModel = [self.listArr objectAtIndex:indexPath.section];
 //    NSArray *tempCellArr = sModel.rowModelArr;
-//    RowModel *rModel = [tempCellArr objectAtIndex:indexPath.row];
+//    RowModel *oldRowModel = [tempCellArr objectAtIndex:indexPath.row];
 //    
-//    rModel = rm;
-//    
-//    if (self.updateCellBlock) {
-//        self.updateCellBlock(indexPath);
-//    }
+//    oldRowModel = newRowModel;
 
-    for (int i = 0; i < self.listArr.count; i++) {
-        SectionModel *sModel = [SectionModel new];
-        if (i == indexPath.section) {
-            sModel = [self.listArr objectAtIndex:i];
-            for (int j = 0; j < sModel.rowModelArr.count; j++) {
-                RowModel *rModel = [RowModel new];
-                if (j == indexPath.row) {
-                    rModel = [sModel.rowModelArr objectAtIndex:j];
-                    rModel.name = @"替换***";
-                    rModel.detail = @"我是被替换的新cell";
-                }
-            }
-        }
+    //2、相当于直接赋值
+//    RowModel *newRM = [RowModel new];
+//    newRM.name = @"新替换的name";
+//    newRM.detail = @"新替换的detail";
+//    
+//    SectionModel *sModel = [SectionModel new];
+//    sModel = [self.listArr objectAtIndex:indexPath.section];
+//    NSArray *tempCellArr = sModel.rowModelArr;
+//    RowModel *oldRM = [tempCellArr objectAtIndex:indexPath.row];
+//    oldRM.name = newRM.name;
+//    oldRM.detail = newRM.detail;
+    
+    //3、和第二种方法一样的道理
+    RowModel *newRowModel = [RowModel new];
+    newRowModel.name = @"新替换的name";
+    newRowModel.detail = @"新替换的detail";
+    
+    SectionModel *sModel = [SectionModel new];
+    sModel = [self.listArr objectAtIndex:indexPath.section];
+    NSArray *tempCellArr = sModel.rowModelArr;
+    self.oldRowModel = [tempCellArr objectAtIndex:indexPath.row];
+//    self.oldRowModel = newRowModel;
+    self.oldRowModel.name = newRowModel.name;
+    self.oldRowModel.detail = newRowModel.detail;
+    
+    
+    if (self.updateCellBlock) {
+        self.updateCellBlock(indexPath);
     }
+    
+    
+    
+//    for (int i = 0; i < self.listArr.count; i++) {
+//        SectionModel *sModel = [SectionModel new];
+//        if (i == indexPath.section) {
+//            sModel = [self.listArr objectAtIndex:i];
+//            for (int j = 0; j < sModel.rowModelArr.count; j++) {
+//                RowModel *rModel = [RowModel new];
+//                if (j == indexPath.row) {
+//                    rModel = [sModel.rowModelArr objectAtIndex:j];
+//                    rModel.name = @"替换***";
+//                    rModel.detail = @"我是被替换的新cell";
+//                }
+//            }
+//        }
+//    }
     
 }
 
